4c05351454069cff00775e61f327f61d695d7cf3
Lillian Mo <molillian05@gmail.com>
2024-07-15 20:56:57 -0400

    example of cut position data

--- test.py
+++ test.py
@@ -1,22 +1,24 @@
 import os
 import numpy as np
 import json
+import pandas as pd
+import ast
 
 base_directory = "./anonymized-files-wisd"
 file_list = os.listdir(base_directory)
 data = []
+positions = []
 
 
 for i in range(len(file_list)):
     curr_file = open(base_directory+"/"+file_list[i])
     data.append(json.load(curr_file))
+    positions.append(data[i]['samples_ball'])
 
 array = np.array(data)
-
-
-print(array[0])
-
-
+example = np.array(positions[0])
+df = pd.json_normalize(positions[0])
+exampleArray = df.values
 
 
 
